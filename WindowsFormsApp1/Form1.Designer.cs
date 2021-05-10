f820ff380e537ac02f77472852eec0bd1746fcea
Dule888811 <dusan.pavlovic88@yahoo.com>
2021-05-10 17:30:57 +0200

    First commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FoldersAndF = new System.Windows.Forms.TreeView();
+            this.buttonFolderFiles = new System.Windows.Forms.Button();
+            this.createXmlFile = new System.Windows.Forms.Button();
+            this.dataFolder = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // FoldersAndF
+            // 
+            this.FoldersAndF.Location = new System.Drawing.Point(1, 1);
+            this.FoldersAndF.Name = "FoldersAndF";
+            this.FoldersAndF.Size = new System.Drawing.Size(495, 449);
+            this.FoldersAndF.TabIndex = 0;
+            // 
+            // buttonFolderFiles
+            // 
+            this.buttonFolderFiles.Location = new System.Drawing.Point(593, 415);
+            this.buttonFolderFiles.Name = "buttonFolderFiles";
+            this.buttonFolderFiles.Size = new System.Drawing.Size(130, 23);
+            this.buttonFolderFiles.TabIndex = 1;
+            this.buttonFolderFiles.Text = "FolderAndFiles";
+            this.buttonFolderFiles.UseVisualStyleBackColor = true;
+            this.buttonFolderFiles.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // createXmlFile
+            // 
+            this.createXmlFile.Location = new System.Drawing.Point(512, 415);
+            this.createXmlFile.Name = "createXmlFile";
+            this.createXmlFile.Size = new System.Drawing.Size(75, 23);
+            this.createXmlFile.TabIndex = 3;
+            this.createXmlFile.Text = "xmlFile";
+            this.createXmlFile.UseVisualStyleBackColor = true;
+            // 
+            // dataFolder
+            // 
+            this.dataFolder.FullRowSelect = true;
+            this.dataFolder.GridLines = true;
+            this.dataFolder.HideSelection = false;
+            this.dataFolder.Location = new System.Drawing.Point(542, 1);
+            this.dataFolder.Name = "dataFolder";
+            this.dataFolder.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataFolder.Size = new System.Drawing.Size(246, 408);
+            this.dataFolder.TabIndex = 4;
+            this.dataFolder.UseCompatibleStateImageBehavior = false;
+            this.dataFolder.View = System.Windows.Forms.View.Details;
+           // this.dataFolder.SelectedIndexChanged += new System.EventHandler(this.dataFolder_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataFolder);
+            this.Controls.Add(this.createXmlFile);
+            this.Controls.Add(this.buttonFolderFiles);
+            this.Controls.Add(this.FoldersAndF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView FoldersAndF;
+        private System.Windows.Forms.Button buttonFolderFiles;
+        private System.Windows.Forms.Button createXmlFile;
+        private System.Windows.Forms.ListView dataFolder;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBO = new FolderBrowserDialog();
+            if (FBO.ShowDialog() == DialogResult.OK)
+            {
+
+                try
+                {
+                    Folderdata(FBO.SelectedPath);
+                    LisrDirectory(FoldersAndF, FBO.SelectedPath);
+                    CreateXml(FBO.SelectedPath);
+
+                }
+                
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+
+
+
+        private void LisrDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+
+
+            var DirectoryNode = new TreeNode(directoryInfo.Name);
+
+            foreach (var directory in directoryInfo.GetDirectories())
+
+                DirectoryNode.Nodes.Add(CreateDirectoryNode(directory));
+
+
+            foreach (var files in directoryInfo.GetFiles())
+
+
+                DirectoryNode.Nodes.Add(new TreeNode(files.Name));
+
+
+            return DirectoryNode;
+
+        }
+
+
+        void CreateXml(string pathToFolder)
+        {
+            int i = 0;
+            long length = 0;
+            var rootDirInfo = new DirectoryInfo(pathToFolder);
+                using (XmlWriter writer = XmlWriter.Create("folders.xml"))
+                {
+                XmlWriterSettings xmlWriterSettings = new XmlWriterSettings { ConformanceLevel = ConformanceLevel.Fragment };
+
+                XmlWriterSettings settings = new XmlWriterSettings();
+                    settings.OmitXmlDeclaration = true;
+                    settings.ConformanceLevel = ConformanceLevel.Fragment;
+                    settings.CloseOutput = false;
+                writer.WriteStartElement(rootDirInfo.Name.ToString());
+                if (rootDirInfo.GetDirectories().Count() > 0)
+                {
+                    foreach (var directory in rootDirInfo.GetDirectories())
+                    {
+                       
+                        writer.WriteStartElement("folderName", directory.Name.ToString());
+
+                        foreach (var files in directory.GetFiles())
+                        {
+                            length += files.Length;
+                            i++;
+                            writer.WriteStartElement("fileName", files.Name);
+                            writer.WriteElementString("FileSize", files.Length.ToString());
+                            writer.WriteElementString("CreationTime", files.CreationTime.ToString());
+                            writer.WriteElementString("fileLastAccessTime", files.LastAccessTime.ToString());
+                            writer.WriteElementString("FileLastModifiedTime", files.LastWriteTime.ToString());
+                            writer.WriteEndElement();
+                        }
+                    }
+                    writer.WriteElementString("FolderSize", length.ToString());
+                    writer.WriteElementString("NuberOfFiles", i.ToString());
+                    writer.WriteEndElement();
+
+                }
+                
+                    foreach (var files in rootDirInfo.GetFiles())
+                    {
+                        writer.WriteStartElement(rootDirInfo.Name.ToString());
+                        writer.WriteStartElement("fileName", files.Name);
+                        writer.WriteElementString("FileSize", files.Length.ToString());
+                        writer.WriteElementString("CreationTime", files.CreationTime.ToString());
+                        writer.WriteElementString("fileLastAccessTime", files.LastAccessTime.ToString());
+                        writer.WriteElementString("FileLastModifiedTime", files.LastWriteTime.ToString());
+                        writer.WriteEndElement();
+                    }
+                
+               
+
+
+
+                writer.WriteEndElement();
+               
+                  
+                     writer.WriteEndDocument();
+                     writer.Flush();
+                     writer.Close();
+
+
+            } 
+
+        
+        }
+
+        void Folderdata(string path)
+        {
+            
+            var rootFolder = new DirectoryInfo(path);
+            dataFolder.Columns.Add("Folder name", 77, HorizontalAlignment.Center);
+            dataFolder.Columns.Add("Folder size", 66, HorizontalAlignment.Center);
+            dataFolder.Columns.Add("Number of files", 88, HorizontalAlignment.Center);
+            ListViewItem eachRow = new ListViewItem(rootFolder.Name);
+            long length = 0;
+            int i = 0;
+          
+            
+                foreach (var files in rootFolder.GetFiles())
+                {
+                    length += files.Length;
+                    i++;
+                }
+            
+           
+
+                
+                
+          
+            ListViewItem.ListViewSubItem folderSize = new ListViewItem.ListViewSubItem(eachRow, length.ToString());
+                ListViewItem.ListViewSubItem numberOfFiles = new ListViewItem.ListViewSubItem(eachRow, i.ToString());
+                eachRow.SubItems.Add(folderSize);
+                eachRow.SubItems.Add(numberOfFiles);
+                dataFolder.Items.Add(eachRow);
+            }
+   
+
+    }
+}
+
+
